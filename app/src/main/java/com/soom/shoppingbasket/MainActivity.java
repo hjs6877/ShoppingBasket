b4d63e175d40cdc569e2e82ecd45e4a70891ebf2
Kevin Hwang <hjs6877@gmail.com>
2016-12-15 10:21:50 +0900

    아이템 삭제 메뉴를 위한 앱바 추가.

--- app/src/main/java/com/soom/shoppingbasket/MainActivity.java
+++ app/src/main/java/com/soom/shoppingbasket/MainActivity.java
@@ -1,9 +1,10 @@
 package com.soom.shoppingbasket;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +16,6 @@ import com.soom.shoppingbasket.database.DBController;
 import com.soom.shoppingbasket.database.SQLData;
 import com.soom.shoppingbasket.model.CartItem;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -34,11 +34,51 @@ public class MainActivity extends AppCompatActivity {
     private List<CartItem> cartItemList;
     private CartItemListAdapter adapter;
 
+    private class ItemAddClickListener implements View.OnClickListener{
+        private Context context;
+
+        public ItemAddClickListener(Context context){
+            this.context = context;
+        }
+
+        /**
+         * DB에 아이템 저장.
+         *
+         * @param v
+         */
+        @Override
+        public void onClick(View v) {
+            insertItem();
+        }
+
+        private void insertItem() {
+            String itemText = editItemText.getEditableText().toString();
+            if(itemText.isEmpty()){
+                Toast.makeText(context, R.string.toast_no_input_item, Toast.LENGTH_SHORT).show();
+            }else{
+                // DB에 아이템 추가
+                dbController.openDb();
+                dbController.insertData(SQLData.SQL_INSERT_ITEM, new CartItem(0, 0, itemText));
+                dbController.closeDb();
+
+                // 리스트뷰에 아이템 추가 및 갱신
+                cartItemList.add(new CartItem(0, 0, itemText));
+                adapter.notifyDataSetChanged();
+
+                // editText의 텍스트 지워서 초기화.
+                editItemText.setText(null);
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 앱바 추가
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         dbController = new DBController(this);
         dbController.openDb();
@@ -55,38 +95,9 @@ public class MainActivity extends AppCompatActivity {
         buttonAdd.setOnClickListener(new ItemAddClickListener(this));
     }
 
-
-    class ItemAddClickListener implements View.OnClickListener{
-        private Context context;
-
-        public ItemAddClickListener(Context context){
-            this.context = context;
-        }
-
-        @Override
-        public void onClick(View v) {
-            String itemText = editItemText.getText().toString();
-            if(itemText.isEmpty()){
-                Toast.makeText(context, R.string.toast_no_input_item, Toast.LENGTH_SHORT).show();
-            }else{
-                /**
-                 * TODO
-                 * - DB에 insert
-                 * - 리스트뷰 갱신
-                 * - EditText 입력 텍스트 지우기
-                 */
-                // DB에 아이템 추가
-                dbController.openDb();
-                dbController.insertData(SQLData.SQL_INSERT_ITEM, new CartItem(0, 0, itemText));
-                dbController.closeDb();
-
-                // 리스트뷰에 아이템 추가 및 갱신
-                cartItemList.add(new CartItem(0, 0, itemText));
-                adapter.notifyDataSetChanged();
-
-                // editText의 텍스트 지워서 초기화.
-                editItemText.setText(null);
-            }
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
